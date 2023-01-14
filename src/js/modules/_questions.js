d046cf5535ab3055b15502e8477368b72394e028
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-01-14 06:50:33 +0300

    feat: adaptive

--- src/js/modules/_questions.js
+++ src/js/modules/_questions.js
@@ -1,34 +1,33 @@
 'use strict'
 
 export function _questions() {
-	const arr = document.querySelectorAll('.questions__ask');
-	if (arr.length > 0) {
-		const newArray = Array.from(arr);
+	const arrButtons = document.querySelectorAll('.questions__ask');
+	const arrAnswer = document.querySelectorAll('.questions__answer')
+
+	if (arrButtons.length > 0) {
+		const buttons = Array.from(arrButtons);
+		const answers = Array.from(arrAnswer);
 
 		const initSpoiler = (arr) => {
 			arr.forEach(elem => {
 				elem.classList.add('_init');
-				initSpoilerBody(elem);
-				elem.addEventListener('click', setSpoiler);
 			})
 		}
 
-		const initSpoilerBody = (elem) => {
-			elem.removeAttribute('tabindex');
-			if(!elem.classList.contains('_active')) {
-				elem.nextElementSibling.hidden = true;
-			}
-		}
+		buttons.forEach(el => {
+			el.addEventListener('click', () => {
+				let answer = el.nextElementSibling;
 
-		const setSpoiler = (event) => {
-			const el = event.target;
-			if (el.classList.contains('questions__ask')) {
-					const spoilerBlock = el.nextElementSibling;
-					spoilerBlock.classList.toggle('_active');
+				if(answer.classList.contains('_active')) {
+					answers.forEach((el) => el.classList.remove('_active'));
 					el.classList.toggle('_active');
+				} else {
+					answers.forEach((el) => el.classList.remove('_active'));
+					el.classList.toggle('_active');
+					answer.classList.add('_active');
 				}
-			}
-
-		initSpoiler(newArray);
+			})
+		})
+		initSpoiler(buttons);
 	}
 }
