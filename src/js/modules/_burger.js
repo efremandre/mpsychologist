e658d1bb427092c5483022cdcf453d968d8ad4c3
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-01-19 03:31:45 +0300

    окончательные правки. Проект сдал.

--- src/js/modules/_burger.js
+++ src/js/modules/_burger.js
@@ -6,6 +6,7 @@ export function _burger() {
 	const navItems = nav?.querySelectorAll('.nav__link');
 	const body = document.body;
 	const header = document?.querySelector('.header');
+	const pressUp = document?.querySelector('.button-up');
 	const headerHeight = header.offsetHeight;
 	document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 
@@ -13,6 +14,7 @@ export function _burger() {
 		body.classList.toggle('stop-scroll');
 		burger?.classList.toggle('_active');
 		nav?.classList.toggle('_active');
+		pressUp?.classList.toggle('_hidden-button');
 	});
 
 	navItems.forEach(el => {
@@ -20,6 +22,7 @@ export function _burger() {
 			body.classList.remove('stop-scroll');
 			burger?.classList.remove('_active');
 			nav?.classList.remove('_active');
+			pressUp?.classList.remove('_hidden-button');
 		});
 	});
 }
--- src/js/modules/_popUp.js
+++ src/js/modules/_popUp.js
@@ -1,21 +1,20 @@
 'use strict'
 
-export function _feedbackButton() {
+export function _popUp() {
     const body = document.querySelector('body');
     const wrapper = document.querySelector('.wrapper');
     const button = document.querySelectorAll('[data-popupBtn]');
     const image = document.querySelector('[data-imgPopup]');
     const popup = document.querySelector('.popup');
+    const pressUp = document?.querySelector('.button-up');
     const scrollWidth = window.innerWidth - wrapper.offsetWidth;
-    const scrollWidthPopup = window.innerWidth - popup.offsetWidth;
-
-
 
     button.forEach(el => {
         el.addEventListener('click', (event) => {
             let elementEvent = event.target;
             let srcImg = el.previousElementSibling.src;
             if (elementEvent) {
+                pressUp?.classList.add('_hidden-button');
                 popup.classList.add('_popup-visible');
                 image.src = srcImg;
                 body.classList.add('stop-scroll');
@@ -25,6 +24,7 @@ export function _feedbackButton() {
     })
 
     popup.addEventListener('click', () => {
+        pressUp?.classList.remove('_hidden-button');
         popup.classList.remove('_popup-visible');
         body.classList.remove('stop-scroll');
         body.style.paddingRight = `0px`;
